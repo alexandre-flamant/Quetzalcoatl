21731070f7f2cb68eae3b9c47e37650d4d078618
alexandre-flamant <alexandreflamant97@gmail.com>
2024-07-27 19:19:26 +0200

    Save

--- src/index.js
+++ src/index.js
@@ -1,51 +1,25 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
+const { SFTPClient } = require( "./sftp.js" )
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
+async function test(){
+    client = new SFTPClient()
+    options = {
+        host: '10.11.99.1',
+        username:'root',
+        password:'fZ5WkZ5VmC',
+        port:22
+    }
+    await client.connect(options)
+    await sleep(5000)
+    client.disconnect()
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
-
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+client = new SFTPClient()
+options = {
+    host: '10.11.99.1',
+    username:'root',
+    password:'fZ5WkZ5VmC',
+    port:22
+}
+await client.connect(options)
+list = client.listFiles()
---
+++ src/tools.js
@@ -0,0 +1,3 @@
+XOCHITL_PATH = "/home/root/.local/share/remarkable/xochitl"
+
+module.exports = { XOCHITL_PATH }
