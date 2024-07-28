3b0917ba0acb3ff062562726377b5f85efa01137
alexandre-flamant <alexandreflamant97@gmail.com>
2024-07-28 23:50:58 +0200

    Global variables and custom types

--- src/global.ts
+++ src/global.ts
@@ -2,9 +2,25 @@
 export const XOCHITL_PATH:string = "/home/root/.local/share/remarkable/xochitl"
 
 // Types
-export type ConnectionOptions = {
+/**
+ * ConnectOption without the port as SFTP default port is 22
+ */
+export type DefaultConnectOptions = {
     host:string, 
     username:string, 
-    password:string, 
-    port:Number
+    password:string,
 };
+
+/**
+ * Content of .metadata files
+ */
+export type Metadata = {
+    createdTime: string,
+    lastModified: string,
+    lastOpened: string,
+    lastOpenedPage: number,
+    parent: string,
+    pinned: boolean,
+    type: string,
+    visibleName: string
+}
